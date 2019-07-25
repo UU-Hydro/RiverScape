f1a8234e304f86a2cc39ab25da2af23a89f72e99
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-07-25 14:09:07 +0200

    continue scratch-ing

--- scratch/measures_py3.py
+++ scratch/measures_py3.py
@@ -1028,7 +1028,7 @@ class RiverMeasures(object):
         for ID in flpl_IDs[:]:
             print(ID)
             flpl_section = pcr.ifthen(
-                                self.r.geom.flpl_wide == pcr.nominal(np.int(ID)),
+                                self.r.geom.flpl_wide == pcr.nominal(int(ID)),
                                 pcr.boolean(1))
             channel = self.side_channel_positioning(friction, flpl_section)
             center_lines = pcr.cover(center_lines, 
