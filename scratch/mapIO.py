047c38d51f2265d61546bf9206fae0cecfb4a967
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-07-25 13:27:01 +0200

    continue scratch-ing

--- scratch/mapIO.py
+++ scratch/mapIO.py
@@ -5,7 +5,7 @@ import subprocess
 import collections
 import ogr, gdal
 import numpy as np
-import pysal as ps
+#~ import pysal as ps # NOT NEEDED
 import pandas as pd
 import geopandas as gpd
 from shapely.geometry import mapping, Point, LineString, Polygon, MultiPolygon
@@ -14,44 +14,44 @@ from shapely.geometry import mapping, Point, LineString, Polygon, MultiPolygon
 #%% IO with vector data #########################################################
 ################################################################################
 
-def dbf2df(dbf_path, index=None, cols=False, incl_index=False):
-    '''
-    Read a dbf file as a pandas.DataFrame, optionally selecting the index
-    variable and which columns are to be loaded.
-    __author__  = "Dani Arribas-Bel <darribas@asu.edu> "
-    ...
-    Arguments
-    ---------
-    dbf_path    : str
-                  Path to the DBF file to be read
-    index       : str
-                  Name of the column to be used as the index of the DataFrame
-    cols        : list
-                  List with the names of the columns to be read into the
-                  DataFrame. Defaults to False, which reads the whole dbf
-    incl_index  : Boolean
-                  If True index is included in the DataFrame as a
-                  column too. Defaults to False
-    Returns
-    -------
-    df          : DataFrame
-                  pandas.DataFrame object created
-    '''
-    db = ps.open(dbf_path)
-    if cols:
-        if incl_index:
-            cols.append(index)
-        vars_to_read = cols
-    else:
-        vars_to_read = db.header
-    data = dict([(var, db.by_col(var)) for var in vars_to_read])
-    if index:
-        index = db.by_col(index)
-        db.close()
-        return pd.DataFrame(data, index=index)
-    else:
-        db.close()
-        return pd.DataFrame(data)
+#~ def dbf2df(dbf_path, index=None, cols=False, incl_index=False):
+    #~ '''
+    #~ Read a dbf file as a pandas.DataFrame, optionally selecting the index
+    #~ variable and which columns are to be loaded.
+    #~ __author__  = "Dani Arribas-Bel <darribas@asu.edu> "
+    #~ ...
+    #~ Arguments
+    #~ ---------
+    #~ dbf_path    : str
+                  #~ Path to the DBF file to be read
+    #~ index       : str
+                  #~ Name of the column to be used as the index of the DataFrame
+    #~ cols        : list
+                  #~ List with the names of the columns to be read into the
+                  #~ DataFrame. Defaults to False, which reads the whole dbf
+    #~ incl_index  : Boolean
+                  #~ If True index is included in the DataFrame as a
+                  #~ column too. Defaults to False
+    #~ Returns
+    #~ -------
+    #~ df          : DataFrame
+                  #~ pandas.DataFrame object created
+    #~ '''
+    #~ db = ps.open(dbf_path)
+    #~ if cols:
+        #~ if incl_index:
+            #~ cols.append(index)
+        #~ vars_to_read = cols
+    #~ else:
+        #~ vars_to_read = db.header
+    #~ data = dict([(var, db.by_col(var)) for var in vars_to_read])
+    #~ if index:
+        #~ index = db.by_col(index)
+        #~ db.close()
+        #~ return pd.DataFrame(data, index=index)
+    #~ else:
+        #~ db.close()
+        #~ return pd.DataFrame(data)
 
 def vector2raster(fileFormat, cloneFile, inFile, attribute, layer = None):
     """Rasterize a vector dataset using the map attributes of the cloneFile.
