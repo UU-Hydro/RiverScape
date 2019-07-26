c642c5ba835d23bac1875de59950e936ce1801a6
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-07-26 06:41:45 +0200

    continue scratch-ing

--- scratch/measures_py3.py
+++ scratch/measures_py3.py
@@ -436,7 +436,6 @@ def write_measure(msr, msr_root_dir):
     """
     settings = msr.settings
     # Create the directory
-    # msr_dir_name = string.join([settings['msr_type'], settings['ID']], sep='_')
     msr_dir_name = settings['msr_type'] + "_" +  settings['ID']
     msr_dir_path = os.path.join(msr_root_dir, msr_dir_name)
     pcrRecipes.make_dir(msr_dir_path)
