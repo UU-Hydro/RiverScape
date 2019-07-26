f060bda0dbb7bd5344e893d1bb64f1ad7c631df5
Edwin Sutanudjaja <sutanudjaja@cehs-hp>
2019-07-26 07:20:48 +0200

    continue scratch-ing

--- scratch/measures_py3.py
+++ scratch/measures_py3.py
@@ -437,7 +437,7 @@ def write_measure(msr, msr_root_dir):
     settings = msr.settings
     # Create the directory
     #~ msr_dir_name = "_".join([settings['msr_type'], settings['ID'])
-    msr_dir_name = settings['msr_type'] + "_" +  settings['ID']
+    msr_dir_name = str(settings['msr_type']) + "_" +  str(float(settings['ID']))
     msr_dir_path = os.path.join(msr_root_dir, msr_dir_name)
     pcrRecipes.make_dir(msr_dir_path)
     print(msr_dir_path)
@@ -1028,7 +1028,8 @@ class RiverMeasures(object):
         
         print(flpl_IDs)
         
-        for ID in flpl_IDs[:]:
+        #~ for ID in flpl_IDs[:]:
+        for ID in flpl_IDs[:][0:2]:
             print(ID)
             flpl_section = pcr.ifthen(pcr.scalar(self.r.geom.flpl_wide) == float(ID), pcr.boolean(1))
             channel = self.side_channel_positioning(friction, flpl_section)
