16ac02851d5b7f8330f02121e10b199f6f7be03e
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2019-11-22 12:21:06 +0100

    continue scratch-ing for biosafe

--- scratch_biosafe/riverscape/__init__.py
+++ scratch_biosafe/riverscape/__init__.py
@@ -19,7 +19,7 @@ from . import pcrRecipes
 from . import measures_py3 as msr
 
 from . import biosafe_py3 as biosafe
-from . import biosafe_py3 as biosafe
+from . import biosafeIO as bsIO
 
 
 from .ipynb_utils import *
