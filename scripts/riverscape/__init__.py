ae5e3109a5769b005b2aaa0dc54341db137a8f6f
Edwin H. Sutanudjaja <sutan101@eejit.geo.uu.nl>
2019-11-23 21:18:03 +0100

    1st version of biosafe is ready

--- scripts/riverscape/__init__.py
+++ scripts/riverscape/__init__.py
@@ -15,8 +15,11 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 from . import mapIO
 from . import pcrRecipes
+
 from . import measures_py3 as msr
 
+from . import biosafe_py3 as biosafe
+from . import biosafeIO as bsIO
 
 
 from .ipynb_utils import *
