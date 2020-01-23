bbb2d848d4857eff0527c5abec66471117b78f12
Edwin Sutanudjaja <edwinhs@cartesius.surfsara.nl>
2020-01-23 16:29:55 +0100

    good progress

--- scratch_biosafe/riverscape/biosafeIO.py
+++ scratch_biosafe/riverscape/biosafeIO.py
@@ -250,6 +250,13 @@ def dbf2df(dbf_path, index=None, cols=False, incl_index=False):
         db.close()
         return pd.DataFrame(data)   
        
+def show_full_data_frame(data_frame):
+    
+    pd.options.display.max_rows = data_frame.shape[0]
+    pd.options.display.max_columns = data_frame.shape[1]
+    
+    return pd.DataFrame(data_frame)
+
 ###############################################################################
 #-test IO #####################################################################
 ###############################################################################
